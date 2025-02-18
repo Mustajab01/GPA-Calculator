38d40913fc1b0cc2b46ef38d1b3c685d26e530d0
Mustajab Ali <mustajabali.321@gmail.com>
2025-02-19 02:02:26 +0500

    fix deployement issue 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
+	// This tells Next.js that your pages are served from '/GPA-Calculator'
+	basePath: "/GPA-Calculator",
+	// This ensures that all asset URLs are prefixed correctly
+	assetPrefix: "/GPA-Calculator/",
 	images: {
 		unoptimized: true,
 	},
